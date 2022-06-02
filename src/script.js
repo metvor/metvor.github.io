d488245b63d5a29abcc867d4d353da0c5a41206f
Mette Vorraa <mettevorraa@gmail.com>
2022-06-02 19:38:28 +0100

    dynamic reveal elements on scroll down

--- src/script.js
+++ src/script.js
@@ -9,5 +9,19 @@ window.onscroll = function() {
     lastScroll = currentScroll;
 }
 
+function reveal() {
+    var reveals = document.querySelectorAll(".fade-in");
+    
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 300;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}   
 
-  
+window.addEventListener("scroll", reveal);
