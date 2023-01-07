01f9552a2568f5519805a97a15b89768e6da116a
Mette Vorraa <mettevorraa@gmail.com>
2023-01-07 23:36:14 +0000

    Update CSS: About & Projects
    About section: create 3 lists and show individual lists onClick
    Project section: restyle as grid display to streamline design and eliminate srolling time for user

--- src/script.js
+++ src/script.js
@@ -9,6 +9,20 @@ window.onscroll = function() {
     lastScroll = currentScroll;
 }
 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
 function reveal() {
     var reveals = document.querySelectorAll(".fade-in");
     
