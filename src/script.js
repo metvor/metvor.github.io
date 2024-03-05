631bcb07d8692da31b0d38f3e953e42ba2efaca0
Mette Vorraa <mettevorraa@gmail.com>
2024-03-05 21:43:32 +0000

    debug & fix broken js script

--- src/script.js
+++ src/script.js
@@ -1,13 +1,21 @@
-var lastScroll = window.pageYOffset;
-window.onscroll = function() {
-    currentScroll = window.pageYOffset;
-    if (lastScroll > currentScroll) {
-        document.getElementById('navbar').style.top = '0';
-    } else {
-        document.getElementById('navbar').style.top = '-63px';
+
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'flex'
     }
-    lastScroll = currentScroll;
-}
+    
+    function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'none'
+    
+    }
+
+
+    let hide = document.getElementById("hideSidebar");
+    hide.addEventListener('click', hideSidebar);
+
+    let show = document.getElementById("showSidebar");
+    show.addEventListener('click', showSidebar);
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents")
@@ -23,19 +31,3 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-function reveal() {
-    var reveals = document.querySelectorAll(".fade-in");
-    
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 300;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}   
-
-window.addEventListener("scroll", reveal);
