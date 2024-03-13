6655b9a3ed41bff0c06c8a3dd3f26a88c9b6332d
Mette Vorraa <mettevorraa@gmail.com>
2024-03-13 19:17:37 +0000

    fix broken code
    About section was not working properly: user should be able to click on titles and see different content. Debugged code and found the problem: slightly different classes used in HTML, CSS and JS = bit of a mess. Now working as expected

--- src/script.js
+++ src/script.js
@@ -15,17 +15,20 @@ let hide = document.getElementById("hideSidebar");
 let show = document.getElementById("showSidebar");
     show.addEventListener('click', showSidebar);
 
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents")
-
-function opentab(tabname) {
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+    var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName('tab-contents');
+    
+    function opentab(tabname){
+      for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+      }
+      for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-content');
+      }
+      event.currentTarget.classList.add('active-link');
+      document.getElementById(tabname).classList.add('active-content');
+     
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
+    
+    
 
